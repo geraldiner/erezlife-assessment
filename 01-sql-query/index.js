6c70caf97c0d15d7766c0554999b8f1a42343c86
Geraldine <hello@geraldiner.com>
2022-01-30 10:44:20 -0500

    add: 01-sql-query- write out wrote out a small sample example and tried it in pgAdmin for postgresql

--- 01-sql-query/index.js
+++ 01-sql-query/index.js
@@ -22,12 +22,51 @@
  */
 
 // Get student_id and name from the two tables
-SELECT name, student_id
+SELECT student_id, name
   FROM student, application
   WHERE student.id = application.student_id
 
 // Counting applications
-COUNT(score) FROM application
+COUNT(score) AS number_of_apps FROM application
 
 // Sort by number_of_apps, then by name
-ORDER BY number_of_apps DESC, name DESC
+ORDER BY number_of_apps DESC, name ASC
+
+// Display student with no apps as 0 ??
+
+/* 
+student
++----+--------------+----------------+
+| id | name         | address        |
++----+--------------+----------------+
+|  1 | John         |  Apple St      |
+|  2 | Regine       |  Banana Rd     |
+|  3 | Maddex       |  Coconut Creek | 
+|  4 | Jean         |  Dandelion Dr  |
+|  5 | Elixandrea   |  Eggland Blvd  | 
++----+--------------+----------------+
+
+application
++----+----------+---------------+
+| id | score    | student_id    |
++----+----------+---------------+
+|  1 | 79       |  2            |
+|  2 | 64       |  1            |
+|  3 | 64       |  3            | 
+|  4 | 57       |  2            |
+|  5 | 67       |  3            | 
+|  6 | 73       |  5            | 
+|  7 | 94       |  3            |
++----+----------+---------------+
+
+expected output from query
++----+--------------+----------------+
+| id | name         | number_of_apps |
++----+--------------+----------------+
+|  3 | Maddex       |  Apple St      |
+|  2 | Regine       |  Banana Rd     |
+|  5 | Elixandrea   |  Coconut Creek | 
+|  1 | John         |  Dandelion Dr  |
+|  4 | Jean         |  Eggland Blvd  | 
++----+--------------+----------------+
+*/
